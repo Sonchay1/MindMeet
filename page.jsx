c84bf95616b439c900caec6d78230373903217e8
Sonchay <114942137+Sonchay1@users.noreply.github.com>
2025-10-15 01:13:43 +0600

    Add files via upload

--- page.jsx
+++ page.jsx
@@ -1,5 +1,10 @@
-import { SignUp } from "@clerk/nextjs";
+import React from "react";
+import AvailabilityForm from "./_components/availability-form";
+import { getUserAvailability } from "@/actions/availability";
+import { defaultAvailability } from "./data";
 
-export default function Page() {
-  return <SignUp />;
+export default async function AvailabilityPage() {
+  const availability = await getUserAvailability();
+
+  return <AvailabilityForm initialData={availability || defaultAvailability} />;
 }
